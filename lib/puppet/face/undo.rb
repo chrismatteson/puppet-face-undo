fc493a97ebaed2edcc663c914be5a94f18fe1a15
Chris Matteson <chris.matteson@gmail.com>
2017-04-11 22:47:15 +0000

    almost there

--- lib/puppet/face/undo.rb
+++ lib/puppet/face/undo.rb
@@ -1,4 +1,6 @@
+require 'puppet'
 require 'puppet/indirector/face'
+require 'puppet/configurer'
 require 'json'
 require 'yaml'
 
@@ -15,12 +17,11 @@ Puppet::Face.define(:undo, '0.0.1') do
       Here is a ton of more useful information :)
     EOT
 
-    when_invoked do |report|
-      input_yaml = YAML::load(report).resource_statuses.values.find_all { |a| a.changed }
-      Puppet.notice "#{input_yaml}"
+    when_invoked do |report, options|
+      input = Puppet::FileSystem.read(report, :encoding => 'utf-8')
+      input_yaml = YAML::load(input).resource_statuses.values.find_all { |a| a.changed }
       output_json = []
       input_yaml.each do |resource|
-        Puppet.notice "hello #{resource}"
         resource_json = {}
         resource_json["title"] = resource.title
         resource_json["file"] = resource.file
@@ -28,23 +29,26 @@ Puppet::Face.define(:undo, '0.0.1') do
         resource_json["tags"] = JSON.parse(resource.tags.to_json)
         resource_json["type"] = resource.resource_type
         events_json = JSON.parse(resource.events.to_json)
-        Puppet.notice "#{events_json}"
         parameters_json = {}
         events_json.each do |event|
           parameters_json["#{event['property']}"] = event['previous_value']
         end
         resource_json["parameters"] = parameters_json
-        Puppet.notice "#{resource_json}"
         output_json.push(resource_json)
       end
-      Puppet.notice "#{output_json}"
+      catalog = '{"catalog_format":1,"catalog_uuid":"00000000-0000-0000-0000-000000000000","classes":[],"code_id":null,"edges":[],"environment":"production","name":"master.inf.puppet.vm","resources":'
+      catalog << output_json.to_json
+      catalog << ',"tags":[],"version":"1234567890123456789012345678901234567890"}'
+      Puppet.notice "#{catalog}"
       output_file = File.open('/tmp/test.json', 'w+')
-      output_file.write('{"catalog_format":1,"catalog_uuid":"00000000-0000-0000-0000-000000000000","classes":[],"code_id":null,"edges":[],"environment":"production","name":"master.inf.puppet.vm","resources":')
+      output_file.write(catalog)
       output_file.close
-      output_file = File.open('/tmp/test.json', 'a')
-      output_file.write(output_json.to_json)
-      output_file.write(',"tags":[],"version":"1234567890123456789012345678901234567890"}')
-      output_file.close
+      env = Puppet.lookup(:environments).get(Puppet[:environment])
+#      env = "Production" #Puppet.lookup(:current_environment)
+      Puppet.override(:current_environment => env, :loaders => Puppet::Pops::Loaders.new(env)) do
+        configurer = Puppet::Configurer.new
+        configurer.run(:catalog => catalog, :pluginsync => false)
+      end
     end
   end
 end
