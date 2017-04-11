b31b94790e4c1af34b90138e7cb06ad94406be2a
Chris Matteson <chris.matteson@gmail.com>
2017-04-11 20:56:03 +0000

    copied report lines into face

--- lib/puppet/face/undo.rb
+++ lib/puppet/face/undo.rb
@@ -1,4 +1,4 @@
-require 'puppet/face'
+require 'puppet/indirector/face'
 require 'json'
 require 'yaml'
 
@@ -9,24 +9,42 @@ Puppet::Face.define(:undo, '0.0.1') do
 
   action :report do
     summary 'Creates and applies a catalog which reverses reversible changes in a specific report on the local system'
+    arguments "<report>"
 
     description <<-EOT
       Here is a ton of more useful information :)
     EOT
 
-    arguments '<report>'
-
     when_invoked do |report|
-      puppetdb = PuppetDB::Connection.new options[:host], options[:port], !options[:no_ssl]
-      parser = PuppetDB::Parser.new
-      if options[:facts] != ''
-        facts = options[:facts].split(',')
-        factquery = parser.facts_query(query, facts)
-      else
-        facts = [:all]
-        factquery = parser.parse(query, :facts)
+      input_yaml = YAML::load(report).resource_statuses.values.find_all { |a| a.changed }
+      Puppet.notice "#{input_yaml}"
+      output_json = []
+      input_yaml.each do |resource|
+        Puppet.notice "hello #{resource}"
+        resource_json = {}
+        resource_json["title"] = resource.title
+        resource_json["file"] = resource.file
+        resource_json["line"] = resource.line
+        resource_json["tags"] = JSON.parse(resource.tags.to_json)
+        resource_json["type"] = resource.resource_type
+        events_json = JSON.parse(resource.events.to_json)
+        Puppet.notice "#{events_json}"
+        parameters_json = {}
+        events_json.each do |event|
+          parameters_json["#{event['property']}"] = event['previous_value']
+        end
+        resource_json["parameters"] = parameters_json
+        Puppet.notice "#{resource_json}"
+        output_json.push(resource_json)
       end
-      parser.facts_hash(puppetdb.query(:facts, factquery, :extract => [:certname, :name, :value]), facts)
+      Puppet.notice "#{output_json}"
+      output_file = File.open('/tmp/test.json', 'w+')
+      output_file.write('{"catalog_format":1,"catalog_uuid":"00000000-0000-0000-0000-000000000000","classes":[],"code_id":null,"edges":[],"environment":"production","name":"master.inf.puppet.vm","resources":')
+      output_file.close
+      output_file = File.open('/tmp/test.json', 'a')
+      output_file.write(output_json.to_json)
+      output_file.write(',"tags":[],"version":"1234567890123456789012345678901234567890"}')
+      output_file.close
     end
   end
 end
